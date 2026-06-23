0786793df6d319000649e10603cd691a7457d4b9
carrine.s <carrine.s@tc168.cloud>
2026-06-23 11:26:29 +0800

    return

--- python_flask.py
+++ python_flask.py
@@ -83,7 +83,8 @@ def manual_create_bonus(username,platform_type,promotion,ticket_id,amount):
         print(platform_type)
         for platform in platform_type:
             if platform in  ('gi8viet','huamei','tcgdemov3','rollbet','lodibet','jkscus1'):
-                Manual_create_single_with_confirm.main(username,promotion,ticket_id,platform,amount)
+                result = Manual_create_single_with_confirm.main(username,promotion,ticket_id,platform,amount)
+                return result
 
 def manual_create_7_type_tcket(username,promotion,platform_type):
         print(platform_type)
