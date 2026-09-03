0831130f011a4bae113feb942f7109b2c1481183
carrine.s <carrine.s@tc168.cloud>
2026-09-03 15:53:01 +0800

    add free spin ticket

--- auto_create_ticket.py
+++ auto_create_ticket.py
@@ -673,7 +673,7 @@ def get_free_spin_event_id(token,merchantCode):
     response_data=response.json()
     if response_data.get('success'):
         value=response_data.get("value",[])
-        if value.isempty()==True:
+        if len(value)==0:
             result = create_free_spin_event.implement(token,merchantCode)
             if result:
                 logging.info("創建免費旋轉活動成功")
